e321aca1b3fb13f0f5b09afdb713c63cefb15d5d
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-06-13 00:30:45 +0500

    Added input elem

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { TextField , Button } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Cross Sum Calculator</h1>
+      <h1 className='main-header' >Cross Sum Calculator</h1>
+      <div className='input-field' >
+      <TextField variant='filled' color='secondary' label='Enter a number' />
+      <Button variant='contained' color='secondary' >Submit</Button>
+      </div>
     </div>
   );
 }

9b61a56c9bb98141bebb601d5657e09fc2093fab
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-06-13 00:50:41 +0500

    State added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { TextField , Button } from '@material-ui/core';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [inputNumber , setInputNumber] = useState<String>('')
+
+  const handleSubmit = () =>{
+    console.log(Number(inputNumber));
+  }
+
   return (
     <div className="App">
       <h1 className='main-header' >Cross Sum Calculator</h1>
       <div className='input-field' >
-      <TextField variant='filled' color='secondary' label='Enter a number' />
-      <Button variant='contained' color='secondary' >Submit</Button>
+      <TextField variant='filled' color='secondary' label='Enter a number' type='number' value={inputNumber} onChange={(e)=> setInputNumber(e.target.value) } />
+      <Button variant='contained' color='secondary' onClick={handleSubmit} >Submit</Button>
       </div>
       <div className='answer-card' >
         <div style={{margin:'auto'}} >
